7f032d3c68458cb1304685e640ff934999cfa4af
劉敦桐 <zse811022@hotmail.com>
2023-09-08 06:04:25 +0800

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+	public class TodoItem
+	{
+        // 待辦事項的標題
+        public string Title { get; set; }
+
+        // 待辦事項是否完成的標記
+        public bool IsCompleted { get; set; }
+
+        // 建構子
+        public TodoItem(string title)
+        {
+            Title = title;
+            IsCompleted = false; // 預設為未完成
+        }
+
+        // 將待辦事項標記為已完成
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+        }
+
+        // 將待辦事項標記為未完成
+        public void MarkAsIncomplete()
+        {
+            IsCompleted = false;
+        }
+
+        // 返回待辦事項的狀態
+        public override string ToString()
+        {
+            return $"{Title} - {(IsCompleted ? "已完成" : "未完成")}";
+        }
+
+    }
+}
